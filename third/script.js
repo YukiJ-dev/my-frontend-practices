1885789dbb9f51c447fa01842c603ad41ca150e2
Nazar Dev <nazar.kasianenko15@gmail.com>
2021-05-09 15:16:13 +0300

    Finished page

--- third/script.js
+++ third/script.js
@@ -1,9 +1,11 @@
-(function () {
+(function() {
 
     // The logic for mobile menu
-    (function () {
+    (function() {
         let body = document.body;
         let mainNav = body.querySelector("#main-nav");
+
+        let mainElements = [body, mainNav];
 
         let mainMenu = body.querySelector(".main-menu");
         let buttonsCont = body.querySelector(".f-main-header > div:last-child");
@@ -13,7 +15,7 @@
         let openingMenuBtn = body.querySelector(".opening-m-btn");
 
         let menuIsOpen;
-        
+
         // Change main styles for mobile menu
         openingMenuBtn.addEventListener("click", event => {
             menuIsOpen = body.classList.toggle("opend-mobile-menu");
@@ -31,13 +33,29 @@
         openingMenuBtn.addEventListener("click", event => {
             mainMenu.dataset.isHidden = !menuIsOpen;
             buttonsCont.dataset.isHidden = !menuIsOpen;
-           
+
             setTimeout(() => {
                 navElements.forEach(liElement => {
                     liElement.dataset.isHidden = !menuIsOpen;
                 });
             }, 100);
         });
+
+        const mediaFor_1024px = window.matchMedia("(min-width: 64em)");
+
+        let actionForQuery = event => mainElements.forEach(htmlEl => {
+            if (event.matches)
+                htmlEl.classList.remove("opend-mobile-menu");
+            else
+                htmlEl.classList.add("opend-mobile-menu");
+        });
+
+        openingMenuBtn.addEventListener("click", event => {
+            if (menuIsOpen)
+                mediaFor_1024px.addEventListener("change", actionForQuery);
+            else
+                mediaFor_1024px.removeEventListener("change", actionForQuery);
+        })
     })();
 
     // Move user to plans contanier
@@ -47,7 +65,7 @@
 
 
         getStartedBtn.addEventListener("click", event => {
-            window.scrollBy(0, planBlockHeader.offsetTop);
+            window.scrollTo(0, planBlockHeader.offsetTop);
         });
     })();
 
@@ -61,5 +79,27 @@
                 header.nextElementSibling.classList.toggle("is-open");
             });
         });
-    })();  
+    })();
+
+    // The fixing menu on the to of page without css animation
+
+    (function() {
+        let mainNav = document.querySelector("#main-nav");
+        let mainHeader = mainNav.closest("header");
+
+        let getStartedBtn = document.querySelector(".main-button");
+
+        window.addEventListener("scroll", event => {
+            let isScrolled = mainNav.classList.contains("is-scrolled");
+
+            if (window.pageYOffset > getStartedBtn.offsetTop && !isScrolled) {
+                mainHeader.style.height = mainNav.clientHeight + "px";
+                mainNav.classList.add("is-scrolled");
+            } else if (window.pageYOffset <= getStartedBtn.offsetTop && isScrolled) {
+                mainHeader.style.height = "";
+                mainNav.classList.remove("is-scrolled");
+            }
+        });
+    })();
+
 })();
